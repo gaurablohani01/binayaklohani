bfff32ff983761a6dbe8d3d0b28fcd38485c91b2
Gaurab Lohani <132120369+gaurablohani01@users.noreply.github.com>
2024-06-07 20:05:22 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let date = new Date();
 let year = date.getFullYear();
 let year_html = document.body.querySelector('.year');
+let year=document.body.querySelector('.yeara');
 year_html.innerHTML = `${year}`;
+year.innerHTML = `${year}`;
